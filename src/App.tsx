1b6ffb77b47dbebd40e95051c23e5bb926447c82
Vidisha Shah <shahvidisha1996@gmail.com>
2024-01-05 18:41:41 +0100

    Created tasks

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,31 @@
-import React from 'react';
-
+import React, { useState } from 'react';
+import { Todo } from './model'
 import './App.css';
+import InputField from './components/InputField';
+import TodoList from './components/TodoList';
 
-function App() {
+const App: React.FC = () => {
+
+  const [todo, setTodo] = useState<string>("");
+  const [todos, setTodos] = useState<Todo[]>([]);
+
+  const handleAdd = (e:React.FormEvent) => {
+    e.preventDefault();
+    if(todo)
+    {
+      setTodos([...todos, {id : Date.now(), todo, isDone: false}])
+      setTodo("")
+    }
+  }
+
+
   return (
     <div className="App">
-      Hello
+      <span className="heading">
+        Taskify
+      </span>
+    <InputField todo = {todo} setTodo ={setTodo} handleAdd = {handleAdd}/>
+    <TodoList todos = {todos} setTodos={setTodos}/>
     </div>
   );
 }
---
+++ src/components/SingleTodo.tsx
@@ -0,0 +1,52 @@
+import React from 'react'
+import { Todo } from '../model'
+import { AiFillEdit,AiFillDelete } from "react-icons/ai";
+import { MdDone } from "react-icons/md";
+import '../styles/style.css'
+
+interface Props {
+    todo : Todo;
+    todos : Todo[];
+    setTodos : React.Dispatch<React.SetStateAction<Todo[]>>;
+}
+
+const SingleTodo:React.FC<Props> = ({todo,todos,setTodos}) => {
+
+    const handleDone = (id:number) => {
+        //setTodos(todos.map((todo)=> todo.id === id ? {...todo, isDone: !todo.isDone} : todo))
+        
+    }
+
+  return (
+    <form className='todos__single'>
+
+    {
+        todo.isDone ? 
+        (
+        <s className="todos__single--text">
+            {todo.todo}
+        </s>
+        ) : 
+        (
+        <span className="todos__single--text">
+            {todo.todo}
+        </span>
+        )
+    }
+      
+      <div>
+        <span className="icon">
+        <AiFillEdit />
+        </span>
+        <span className="icon">
+        <AiFillDelete />
+        </span>
+        <span className="icon" onClick={()=>handleDone(todo.id)}>
+        <MdDone />  
+        </span>
+      </div>
+    </form>
+  )
+}
+
+export default SingleTodo
